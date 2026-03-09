634592ccf239bb2c38a8463c937a8d6758679dd9
G <gonzalo.conejero@ik.me>
2026-03-09 09:29:51 +0100

    Revert "fix/swipe-to-delete"
    This reverts commit 32904086b095790d1d2afd272500b498b42d1cfc.

--- app.js
+++ app.js
@@ -458,16 +458,6 @@ function displayHistory(data) {
             }
         }, false);
 
-        item.querySelector('.history-item-delete-bg').addEventListener('touchstart', (e) => {
-            e.stopPropagation();
-        });
-        item.querySelector('.history-item-delete-bg').addEventListener('touchend', (e) => {
-            e.stopPropagation();
-            e.preventDefault();
-            if (confirm('¿Deseas eliminar este repostaje?')) {
-                deleteEntry(item.dataset.id);
-            }
-        });
         item.querySelector('.history-item-delete-bg').addEventListener('click', (e) => {
             e.stopPropagation();
             if (confirm('¿Deseas eliminar este repostaje?')) {
